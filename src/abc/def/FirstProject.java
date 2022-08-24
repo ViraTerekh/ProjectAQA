6b25e85a938c34b019a7d7df8714a8f54507268b
ViraTerekh <vira.terekh@gmail.com>
2022-08-25 01:02:04 +0300

    Created 4 mathematics methods.

---
+++ src/abc/def/FirstProject.java
@@ -0,0 +1,23 @@
+//public class FirstProject {
+//    public static void main(String[] args) {
+//        System.out.println("Hello!");
+//    }
+//}
+
+/*package abc.def;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstProject {
+    /*
+     *
+     * @param args comment
+    public static void main(String[] args) {
+        List<Integer> List = new ArrayList<>();
+        for (int i=0; i<args.length; i++) {
+            System.out.println("Arg" + i + "is" + args[i]);
+        }
+    }
+}
+*/
